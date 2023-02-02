ee6d9335317f92bb107b6a79de7ab96d8e6d297f
Muriel Cishek <muriel_cibra@hotmail.com>
2023-02-02 15:29:38 +0100

    Coding-Task: Use operators & data types

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,1 +1,16 @@
+console.log("M" + "u" + "r" + "i" + "e" + "l");
+
 console.log("Hello World");
+// Error! Make it work
+console.log("Hallo");
+// Error! Make it work
+console.log("That doesnt work");
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+// Wrong result: Expected a Number 2 received 22
+console.log(+"2" + 2);
+console.log(25 / 5 === 5 && 50 / 10 === 5);
